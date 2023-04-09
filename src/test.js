68fdfa47cb7d9de550d4d8e3df40181806ec9093
yehoon308 <yehoon308@gmail.com>
2023-04-09 17:42:57 +0900

    test 2

---
+++ src/test.js
@@ -0,0 +1,23 @@
+import React from 'React'
+
+function Test() {
+  return (
+    <div className="Test">
+      <header className="Test-header">
+        <p>
+          Edit <code>src/Test.js</code> and save to reload all.
+        </p>
+        <a
+          className="Test-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Test;
