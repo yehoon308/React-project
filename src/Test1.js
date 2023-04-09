1f6a719b6b880506e9b579ad937af9d4d6f02f5f
yehoon308 <yehoon308@gmail.com>
2023-04-09 17:45:04 +0900

    김종석 작업

---
+++ src/Test1.js
@@ -0,0 +1,23 @@
+import React from 'React'
+function Test1() {
+  return (
+    <div className="Test1">
+      <header className="Test1-header">
+        <img src={logo} className="Test1-logo" alt="logo" />
+        <p>
+          Edit <code>src/Test1.js</code> and save to reload all.
+        </p>
+        <a
+          className="Test1-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Test1;
